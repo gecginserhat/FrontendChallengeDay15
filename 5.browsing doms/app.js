ebcedcd666c56f6fb9a6b53fdbd452bc33bd7c67
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-10 22:03:21 +0300

    Add files via upload

---
+++ 5.browsing doms/app.js
@@ -0,0 +1,51 @@
+let value;
+
+const todoList = document.querySelector(".list-group");
+const todo = document.querySelector(".list-group-item:nth-child(2)");
+const cardrow = document.querySelector(".card.row");
+
+value= todoList;
+value = todo;
+value = cardrow;
+
+// Child Nodes - Text including
+
+value = todoList.childNodes;
+value = todoList.childNodes[0];
+
+// Children - Element
+
+value = todoList.children;
+value = todoList.children[todoList.children.length - 1];
+value = todoList.children[2].textContent = "Değişti";
+
+
+
+value = cardrow;
+value = cardrow.children;
+value = cardrow.children[2].children[1].textContent = "Burası da değişti";
+
+
+value = todoList;
+value = todoList.firstElementChild;
+value = todoList.lastElementChild;
+value = todoList.children.length;
+value = todoList.childElementCount;
+
+value = cardrow;
+value = cardrow.parentElement;
+value = cardrow.parentElement.parentElement;
+
+
+// Element Siblings
+
+value = todo;
+
+value = todo.previousElementSibling;
+value = todo.nextElementSibling;
+value = todo.nextElementSibling.nextElementSibling;
+
+value = todo.previousElementSibling.previousElementSibling;
+console.log(value);
+
+
